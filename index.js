50ef7dfa9586d1804f04b5b86374029141df9278
Eshan7775 <allalamin3288@gmail.com>
2026-07-20 17:15:54 +0600

    Refactor bot startup and update log messages

--- index.js
+++ index.js
@@ -1,37 +1,30 @@
-/**
- * @author NTKhang
- * ! The source code is written by NTKhang, please don't change the author's name everywhere. Thank you for using
- * ! Official source code: https://github.com/ntkhang03/Goat-Bot-V2
- */
-
 const { spawn } = require("child_process");
-const log = require("./logger/log.js");
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Naruto Bot is Running Online 24/7!');
+app.get("/", (req, res) => {
+  res.send("Naruto Bot is Online 24/7!");
 });
 
 app.listen(port, () => {
-  console.log(`Uptime server running on port ${port}`);
+  console.log(`[ SERVER ] Uptime server running on port ${port}`);
 });
 
-function startProject() {
-	const child = spawn("node", ["Goat.js"], {
-		cwd: __dirname,
-		stdio: "inherit",
-		shell: true
-	});
+function startBot() {
+  const child = spawn("node", ["Goat.js"], {
+    cwd: __dirname,
+    stdio: "inherit",
+    shell: true
+  });
 
-	child.on("close", (code) => {
-		if (code == 2) {
-			log.info("Restarting Project...");
-			startProject();
-		}
-	});
+  child.on("close", (code) => {
+    if (code === 2) {
+      console.log("[ SYSTEM ] Restarting Bot Process...");
+      startBot();
+    }
+  });
 }
 
-startProject();
+startBot();
